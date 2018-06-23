00ee78e2fcff3d5a7e42e8b8a94bdea886d55d81
Unknown <krlosx1596@gmail.com>
2018-06-23 03:09:11 +0000

    Hoy si michy

--- comercial/apps/inventario/urls.py
+++ comercial/apps/inventario/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
-from apps.inventario.views import index,productos,categoria,proveedor,marca
+from apps.inventario.views import index,productos,categoria,proveedor, \
+    marca_list,CrearMarca,ModificarMarca,DetalleMarca
 app_name = 'comercial'
 
 urlpatterns = [
     path('dashboard/',index, name = 'dashboard'),
     path('productos/',productos, name = 'productos'),
-    path('marcas/',marca, name = 'marcas'),
+    path('marcas/',marca_list.as_view(), name = 'marca'),
+    path('crear_marca/',CrearMarca.as_view(), name = 'crear_marcas'),
+    path('modificar_marca/(?P<pk>.+)/',ModificarMarca.as_view(), name = 'modificar_marcas'),
+    path('detalle_marca/(?P<pk>.+)/',DetalleMarca.as_view(), name = 'detalle_marcas'),
     path('categorias/',categoria, name = 'categorias'),
     path('proveedores/',proveedor, name = 'proveedor'),
 ]
