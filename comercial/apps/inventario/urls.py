86d1a5a25b2fb439c3d8621410b4ae078339186a
Unknown <krlosx1596@gmail.com>
2018-06-24 02:33:00 +0000

    Algo bueno xD

--- comercial/apps/inventario/urls.py
+++ comercial/apps/inventario/urls.py
@@ -1,11 +1,8 @@
 from django.urls import include, path
 from apps.inventario.views import index,productos,categoria, \
-<<<<<<< Updated upstream
-    marca_list,CrearMarca,ModificarMarca,DetalleMarca,EliminarMarca,proveedor,CrearProveedor,ModificarProveedor,DetalleProveedor, ListadoProveedor
-=======
-    marca_list,CrearMarca,ModificarMarca,DetalleMarca,EliminarMarca,proveedor,CrearProveedor,ModificarProveedor,\
-    DetalleProveedor
->>>>>>> Stashed changes
+    marca_list,CrearMarca,ModificarMarca,DetalleMarca,EliminarMarca,proveedor, \
+    CrearProveedor,ModificarProveedor,DetalleProveedor, ListadoProveedor
+
 from apps.venta.views import ListadoClientes, CrearCliente, ModificarCliente, DetalleCliente
 
 app_name = 'comercial'
@@ -15,19 +12,13 @@ urlpatterns = [
     path('productos/',productos, name = 'productos'),
     path('categorias/',categoria, name = 'categorias'),
     #proveedores
-<<<<<<< Updated upstream
+
     path('proveedores/',ListadoProveedor.as_view(), name = 'proveedor'),
-    path('crear_proveedor/',CrearProveedor.as_view(), name = 'crear_proveedor'),
+    path('crear_proveedor/',CrearProveedor.as_view(), name = 'crear_proveedores'),
     path('modificar_proveedor/(?P<pk>.+)/',ModificarProveedor.as_view(), name = 'modificar_proveedor'),
     path('detalle_proveedor/(?P<pk>.+)/',DetalleProveedor.as_view(), name = 'detalle_proveedor'),
     path('eliminar_proveedor/(?P<pk>.+)/',EliminarMarca.as_view(), name = 'eliminar_proveedor'),
-=======
-    path('proveedores/',proveedor.as_view(), name = 'proveedor'),
-    path('crear_proveedor/',CrearProveedor.as_view(), name = 'crear_proveedores'),
-    path('modificar_provedor/(?P<pk>.+)/',ModificarProveedor.as_view(), name = 'modificar_proveedor'),
-    path('detalle_marca/(?P<pk>.+)/',DetalleProveedor.as_view(), name = 'detalle_proveedor'),
-    path('eliminar_marca/(?P<pk>.+)/',EliminarMarca.as_view(), name = 'eliminar_proveedor'),
->>>>>>> Stashed changes
+
     #Marcas
     path('marcas/',marca_list.as_view(), name = 'marca'),
     path('crear_marca/',CrearMarca.as_view(), name = 'crear_marcas'),
