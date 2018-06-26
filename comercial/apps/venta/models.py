0534ea38699a4e5dd864b8e8ca54779c43049088
Unknown <krlosx1596@gmail.com>
2018-06-26 02:10:24 +0000

    Modelo ventas

--- comercial/apps/venta/models.py
+++ comercial/apps/venta/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length = 50)
     apellido = models.CharField(max_length = 70)
-    direccion = models.CharField(max_length = 80)    
+    direccion = models.CharField(max_length = 80)
     registro = models.CharField(max_length = 25)
     telefono = models.CharField(max_length = 12)
 
-
+    def __str__(self):
+        return '{}'.format(self.nombre)
