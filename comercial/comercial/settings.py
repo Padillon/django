dcc575a84c1792227306c6177f2980503eccccb8
Baionz <baionz_hg@hotmail.com>
2018-06-27 11:33:42 -0600

    a

--- comercial/comercial/settings.py
+++ comercial/comercial/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.inventario',
-    'apps.venta',
 ]
 
 MIDDLEWARE = [
--- comercial/comercial/urls.py
+++ comercial/comercial/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('categorias/', include('apps.inventario.urls', namespace='categorias')),
     path('proveedores/', include('apps.inventario.urls', namespace='proveedor')),
     path('clientes/', include('apps.inventario.urls', namespace='cliente')),
-    path('vender/', include('apps.inventario.urls', namespace='vender')),
+    path('ventas/', include('apps.inventario.urls', namespace='venta')),
 
 ]
